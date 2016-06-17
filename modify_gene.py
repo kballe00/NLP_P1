764d66665bdb614dedd99b966088b06016f093d8
Keith Ballew <kballe00@gmail.com>
2016-06-17 06:21:25 -0700

    Formatted and added comments to modify_gene.py.

--- modify_gene.py
+++ modify_gene.py
@@ -1,13 +1,26 @@
+# Filename: modify_gene.py
+# Author: Keith Ballew
+# Description: Counts the words in gene.train, and replaces words that occur less than
+#              five times with "_RARE_"
+# Usage: python gene.train
+# Date created: 06/16/16
+# Last modified: 06/17/16
+
 gene_train_file = open( 'gene.train', 'r' );
 modified_gene_train_file = open( 'modified_gene.train', 'w' );
 
 dictionary = dict( );
 
+# Iterate over words and count them
 for line in gene_train_file:
     split_line = line.split( );
+
+    # Ignore lines with no text
     if len( split_line ) == 0:
         continue;
+
     word = split_line[ 0 ];
+
     if word in dictionary:
         dictionary[ word ] += 1;
     else:
@@ -16,13 +29,19 @@ for line in gene_train_file:
 # Reset file read position for iterating again
 gene_train_file.seek( 0 );
 
+# Iterate again to replace words that occurred less than five times 
 for line in gene_train_file:
     split_line = line.split( );
+
+    # Ignore lines with no text
     if len( split_line ) == 0:
         continue;
+
     word = split_line[ 0 ];
+
     if dictionary[ word ] < 5:
         split_line[ 0 ] = '_RARE_';
+
     modified_gene_train_file.write( ' '.join( split_line ) + '\n' );
     
 gene_train_file.close( );
